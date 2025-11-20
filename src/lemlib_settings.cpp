7ee312dcbd47fdc5eab4c038d75a6cd6501c0a18
AMRAAMslinger <shaynezheng54@gmail.com>
2025-11-20 13:22:27 -0500

    proper lemlib

--- src/lemlib_settings.cpp
+++
@@ -1,7 +0,0 @@
-#include "lemlib_settings.h"
-
-pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::MotorGroup left_mg({1, -2, 3});
-pros::MotorGroup right_mg({-4, 5, -6});
-
-
--- src/lemlib_settings.h
+++ src/lemlib_settings.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "lemlib/chassis/trackingWheel.hpp"
 #include "main.h"
 #include "lemlib/api.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/optical.hpp"
 
-extern pros::Controller master;
-extern pros::MotorGroup left_mg;
-extern pros::MotorGroup right_mg;
+inline pros::Controller master(pros::E_CONTROLLER_MASTER);
+inline pros::MotorGroup left_mg({1, -2, 3});
+inline pros::MotorGroup right_mg({-4, 5, -6});
+
+inline pros::IMU imu(13);
+inline pros::optical col_en(10);
+
+inline lemlib::Drivetrain drivetrain(&left_mg,&right_mg,
+11.5,
+lemlib::Omniwheel::NEW_275,
+450,
+2
+);
